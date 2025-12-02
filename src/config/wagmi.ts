d59db862d7456fdb3693840db17e94d0c408d2fc
Riccardo Ferranti <riccardo.ferranti01@gmail.com>
2025-12-03 00:19:21 +0700

    Fix chain Id not detected in vercel

--- src/config/wagmi.ts
+++ src/config/wagmi.ts
@@ -1,18 +1,22 @@
-import { cookieStorage, createStorage, createConfig, http, injected } from '@wagmi/core';
+import type { CreateConnectorFn } from '@wagmi/core';
+import { cookieStorage, createStorage, createConfig, http } from '@wagmi/core';
 import * as wagmiChains from '@wagmi/core/chains';
 
 const allChains = Object.values(wagmiChains);
 const firstChain = allChains[0];
 
-let connectors = [injected()];
+let connectors: CreateConnectorFn[]  = [];
 
-// Only load MetaMask and WalletConnect in browser
 if (typeof window !== 'undefined') {
+  const { injected } = require('wagmi/connectors');
   const { metaMask, walletConnect } = require('wagmi/connectors');
   const { PROJECT_ID } = require('@/consts/usdc');
 
-  connectors.push(metaMask());
-  connectors.push(walletConnect({ projectId: PROJECT_ID }));
+  connectors = [
+    injected(),
+    metaMask(),
+    walletConnect({ projectId: PROJECT_ID }),
+  ];
 }
 
 export const config = createConfig({
@@ -25,5 +29,5 @@ export const config = createConfig({
     acc[chain.id] = http();
     return acc;
   }, {} as Record<number, ReturnType<typeof http>>),
-  ssr: true,
+  ssr: false,
 });
