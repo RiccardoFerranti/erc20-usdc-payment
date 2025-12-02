747056be236f62ad8319f454212156b9eb957aaa
Riccardo Ferranti <riccardo.ferranti01@gmail.com>
2025-12-03 00:25:10 +0700

    Fix chain Id not detected in vercel - 1

--- src/config/wagmi.ts
+++ src/config/wagmi.ts
@@ -1,27 +1,27 @@
-import type { CreateConnectorFn } from '@wagmi/core';
-import { cookieStorage, createStorage, createConfig, http } from '@wagmi/core';
+// import type { CreateConnectorFn } from '@wagmi/core';
+import { cookieStorage, createStorage, createConfig, http, injected } from '@wagmi/core';
 import * as wagmiChains from '@wagmi/core/chains';
 
 const allChains = Object.values(wagmiChains);
 const firstChain = allChains[0];
 
-let connectors: CreateConnectorFn[]  = [];
+// let connectors: CreateConnectorFn[]  = [];
 
-if (typeof window !== 'undefined') {
-  const { injected } = require('wagmi/connectors');
-  const { metaMask, walletConnect } = require('wagmi/connectors');
-  const { PROJECT_ID } = require('@/consts/usdc');
+// if (typeof window !== 'undefined') {
+//   const { injected } = require('wagmi/connectors');
+//   const { metaMask, walletConnect } = require('wagmi/connectors');
+//   const { PROJECT_ID } = require('@/consts/usdc');
 
-  connectors = [
-    injected(),
-    metaMask(),
-    walletConnect({ projectId: PROJECT_ID }),
-  ];
-}
+//   connectors = [
+//     injected(),
+//     metaMask(),
+//     walletConnect({ projectId: PROJECT_ID }),
+//   ];
+// }
 
 export const config = createConfig({
   chains: [firstChain, ...allChains.filter(c => c.id !== firstChain.id)],
-  connectors,
+  connectors:[injected()],
   storage: createStorage({
     storage: cookieStorage,
   }),
